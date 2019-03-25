4ba2c4232598a02bf64d50c524377ecd401a83b4
bwrooney <brooney@umass.edu>
2019-03-25 23:44:23 +0000

    fixed

--- src/App01.jsx
+++ src/App01.jsx
@@ -1,7 +1,19 @@
 // This is a place holder for the initial application state.
-const state = [
+const state = {
+  current_text: "",
+  date: "Today",
+  title: ""
+};
 
-];
+const style = {
+  entryBox: {
+
+  },
+  button: {
+    margin: "5px"
+
+  }
+}
 
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
@@ -9,14 +21,44 @@ var contentNode = document.getElementById("contents");
 class JournalEntry extends React.Component {
   constructor() {
     super();
+    this.state = state;
+    this.getDate = this.getDate.bind(this);
+    this.onsave = this.onSave.bind(this);
+  }
+
+  getDate() {
+    const date = new Date();
+    let dd = String(date.getDate()).padStart(2, '0');
+    let mm = String(date.getMonth() + 1).padStart(2, '0'); 
+    let yyyy = date.getFullYear();
+    let hh = date.getHours();
+    let mmm = date.getMinutes();
+    let newDate =  mm + '/' + dd + '/' + yyyy + ' ' + hh + ':' + mmm;
+    return newDate;
+  }
+
+  onSave() {
+    const Ndate = this.getDate();
+    const Ntitle = document.getElementById("titlebox").value;
+    const Nwords = document.getElementById("entrybox").value;
+    this.setState({title: Ntitle, current_text:Nwords, date: Ndate});
+    //alert("Saved!");
   }
 
   render() {
+    console.log(this.state);
+    const viewLink = "./PreviousEntriesView.html";
+    const EditLink = "./EditView.html";
     return (
-      <div>
-        <h1>Current Journal Entry</h1>
-        <textarea rows="40" cols="80" placeholder="Write anything here..." spellcheck="true">
+      <div style={style.entryBox}>
+        <h1 style={style.title}>Current Journal Entry</h1>
+        <p style={style.date}>{this.state.date}</p>
+        <textarea rows="1" id="titlebox" cols="80" placeholder="Write a title here..." >
         </textarea>
+        <textarea rows="40" id="entrybox" cols="80" placeholder="Write anything here...">
+        </textarea>
+      <button style={style.button} onClick={() => {window.location=viewLink}}>Previous Entries</button>
+      <button style={style.button} onClick={() => {this.onSave()}}>Save</button>
       </div>
     );
   }
--- static/App01.js
+++ static/App01.js
@@ -9,10 +9,21 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
 // This is a place holder for the initial application state.
-var state = [];
+var state = {
+  current_text: "",
+  date: "Today",
+  title: ""
+};
 
-// This grabs the DOM element to be used to mount React components.
-var contentNode = document.getElementById("contents");
+var style = {
+  entryBox: {},
+  button: {
+    margin: "5px"
+
+  }
+
+  // This grabs the DOM element to be used to mount React components.
+};var contentNode = document.getElementById("contents");
 
 var JournalEntry = function (_React$Component) {
   _inherits(JournalEntry, _React$Component);
@@ -20,21 +31,72 @@ var JournalEntry = function (_React$Component) {
   function JournalEntry() {
     _classCallCheck(this, JournalEntry);
 
-    return _possibleConstructorReturn(this, (JournalEntry.__proto__ || Object.getPrototypeOf(JournalEntry)).call(this));
+    var _this = _possibleConstructorReturn(this, (JournalEntry.__proto__ || Object.getPrototypeOf(JournalEntry)).call(this));
+
+    _this.state = state;
+    _this.getDate = _this.getDate.bind(_this);
+    _this.onsave = _this.onSave.bind(_this);
+    return _this;
   }
 
   _createClass(JournalEntry, [{
+    key: "getDate",
+    value: function getDate() {
+      var date = new Date();
+      var dd = String(date.getDate()).padStart(2, '0');
+      var mm = String(date.getMonth() + 1).padStart(2, '0');
+      var yyyy = date.getFullYear();
+      var hh = date.getHours();
+      var mmm = date.getMinutes();
+      var newDate = mm + '/' + dd + '/' + yyyy + ' ' + hh + ':' + mmm;
+      return newDate;
+    }
+  }, {
+    key: "onSave",
+    value: function onSave() {
+      var Ndate = this.getDate();
+      var Ntitle = document.getElementById("titlebox").value;
+      var Nwords = document.getElementById("entrybox").value;
+      this.setState({ title: Ntitle, current_text: Nwords, date: Ndate });
+      //alert("Saved!");
+    }
+  }, {
     key: "render",
     value: function render() {
+      var _this2 = this;
+
+      console.log(this.state);
+      var viewLink = "./PreviousEntriesView.html";
+      var EditLink = "./EditView.html";
       return React.createElement(
         "div",
-        null,
+        { style: style.entryBox },
         React.createElement(
           "h1",
-          null,
+          { style: style.title },
           "Current Journal Entry"
         ),
-        React.createElement("textarea", { rows: "40", cols: "80", placeholder: "Write anything here...", spellcheck: "true" })
+        React.createElement(
+          "p",
+          { style: style.date },
+          this.state.date
+        ),
+        React.createElement("textarea", { rows: "1", id: "titlebox", cols: "80", placeholder: "Write a title here..." }),
+        React.createElement("textarea", { rows: "40", id: "entrybox", cols: "80", placeholder: "Write anything here..." }),
+        React.createElement(
+          "button",
+          { style: style.button, onClick: function onClick() {
+              window.location = viewLink;
+            } },
+          "Previous Entries"
+        ),
+        React.createElement(
+          "button",
+          { style: style.button, onClick: function onClick() {
+              _this2.onSave();
+            } },
+          "Save"
+        )
       );
     }
   }]);
