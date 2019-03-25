2300dd7a49c2b1a4aa61a9389dc21e3f0e572eb5
RyanPSheehan <43252016+RyanPSheehan@users.noreply.github.com>
2019-03-25 16:03:15 -0400

    Add files via upload

--- navbar.jsx
+++ navbar.jsx
@@ -6,21 +6,35 @@ const state = [
 // This grabs the DOM element to be used to mount React components.
 var contentNode = document.getElementById("contents");
 
-class JournalEntry extends React.Component {
+class NavigationBar extends React.Component {
   constructor() {
     super();
   }
 
   render() {
     return (
-      <div>
-        <h1>Current Journal Entry</h1>
-        <textarea rows="40" cols="80" placeholder="Write anything here..." spellcheck="true">
-        </textarea>
-      </div>
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+       <a class="navbar-brand" href="#">The Notebook</a>
+       <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+       <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+             <a class="nav-link" href="App01.jsx">Current Journal Entry</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="App02.jsx">Previous Entries</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="App03.jsx">Previous Journal Entry</a>
+           </li>
+         </ul>
+        </div>
+     </nav>
     );
   }
 }
 
-// This renders the JSX component inside the content node:
-ReactDOM.render(<JournalEntry />, contentNode);
+ReactDOM.render(<NavigationBar />, contentNode);
